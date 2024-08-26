35b373621183ea8c60233c3bc8b99ba72425dc3d
Elena G Blanco <elena.guzbla@gmail.com>
2024-08-26 11:38:13 +0000

    ✨ Start project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Almería 💘 Málaga
+
+Uniendo talento y rompiendo barreras
+
+![Portada del evento](https://media.licdn.com/dms/image/v2/D4D24AQGo-SvKzmQQFw/video-liveannouncement-shrink_1280_720/video-liveannouncement-shrink_1280_720/0/1724419584928?e=1725278400&v=beta&t=WlSioGxN1szO0Dm8ibAUNF935R8IX2biF1qaQi03U6w)
+
+Enlaces de interés:
+
+- [Evento en LinkedIn](https://www.linkedin.com/events/7232739967683198976)
+- [Almería Tech en Twitter](https://x.com/AlmeriaTech_es)
+- [Exeal en Twitter](https://x.com/exeal)
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Almería 💘 Málaga',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Almería 💘 Málaga')),
+      body: const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
